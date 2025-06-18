fa4426a5f40d4d7b0c59b99f685d100ee9037f4d
UnishRzk <unishrajak0@gmail.com>
2025-06-18 19:07:31 +0545

    Created CLI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,125 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Main{
     public static void main(String[] args) {
 
+        Main main = new Main();
+        StudentManager manager = new StudentManager();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            main.header();
+            System.out.println("1. Add Student");
+            System.out.println("2. Display All Student");
+            System.out.println("3. Find Student By Id");
+            System.out.println("4. Update Student");
+            System.out.println("5. Remove Student");
+            System.out.println("0. Exit");
+            System.out.println("Choose an option: ");
+
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice){
+                case 1:
+                    System.out.print("Enter ID: ");
+                    String id = sc.nextLine();
+
+                    System.out.print("Enter Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Enter Age: ");
+                    int age = sc.nextInt();
+
+                    System.out.print("Enter GPA: ");
+                    double gpa = sc.nextDouble();
+                    sc.nextLine();
+
+                    System.out.println("Enter the number of courses: ");
+                    int n_courses = sc.nextInt();
+
+                    ArrayList<String> courses = new ArrayList<>();
+                    for (int i = 0; i < n_courses; i++) {
+                        System.out.println("Enter Course "+ (i+1) + ": ");
+                        courses.add(sc.nextLine());
+                    }
+
+                    Student student = new Student(id,name,age,courses,gpa);
+                    manager.addStudent(student);
+                    break;
+
+                case 2:
+                    manager.displayAllStudents();
+                    break;
+
+                case 3:
+                    System.out.println("Enter the Student id: ");
+                    String searchid = sc.nextLine();
+                    Student found = manager.findById(searchid);
+
+                    if (found != null) {
+                        System.out.println(found);
+                    } else {
+                        System.out.println("Student not found!");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Enter ID to update: ");
+                    String updateId = sc.nextLine();
+
+                    System.out.print("Enter new name: ");
+                    String newName = sc.nextLine();
+
+                    System.out.print("Enter new age: ");
+                    int newAge = sc.nextInt();
+
+                    System.out.print("Enter new GPA: ");
+                    double newGpa = sc.nextDouble();
+                    sc.nextLine();
+
+                    manager.updateStudent(updateId,newName,newAge,newGpa);
+                    break;
+
+                case 5:
+                    System.out.println("Enter ID to remove: ");
+                    String removeId = sc.nextLine();
+                    manager.removeStudent(removeId);
+                    System.out.println("Student Removed Sucessfully");
+                    break;
+
+                case 0:
+                    System.out.println("Exiting");
+                    sc.close();
+                    System.exit(0);
+                    break;
+
+                default:
+                    System.out.println("Invalid Choice Please Try again");
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+    public  void header(){
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+        System.out.println(" _____ _             _            _    ___  ___                _     _____           _                 \n" +
+                "/  ___| |           | |          | |   |  \\/  |               | |   /  ___|         | |                \n" +
+                "\\ `--.| |_ _   _  __| | ___ _ __ | |_  | .  . | __ _ _ __ ___ | |_  \\ `--. _   _ ___| |_ ___ _ __ ___  \n" +
+                " `--. \\ __| | | |/ _` |/ _ \\ '_ \\| __| | |\\/| |/ _` | '_ ` _ \\| __|  `--. \\ | | / __| __/ _ \\ '_ ` _ \\ \n" +
+                "/\\__/ / |_| |_| | (_| |  __/ | | | |_  | |  | | (_| | | | | | | |_  /\\__/ / |_| \\__ \\ ||  __/ | | | | |\n" +
+                "\\____/ \\__|\\__,_|\\__,_|\\___|_| |_|\\__| \\_|  |_/\\__, |_| |_| |_|\\__| \\____/ \\__, |___/\\__\\___|_| |_| |_|\n" +
+                "                                                __/ |                       __/ |                      \n" +
+                "                                               |___/                       |___/                       ");
+        System.out.println("---------------------------------------------------------------------------------------------------------");
+
     }
 }
+
---
+++ src/Playground.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Playground {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        for (int i = 0; i < 5; i++) {
+            double a = sc.nextDouble();
+            System.out.println(a);
+
+        }
+
+    }
+}
